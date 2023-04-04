414aad167e9208554de36df78695b0db1f3e6344
Tri Nguyen <nguyenmtri11@gmail.com>
2023-04-04 11:25:43 +0700

    Send raw text only

--- main.py
+++ main.py
@@ -25,15 +25,19 @@ def webhook():
     try:
         if request.method == "POST":
             data = request.get_json()
-            key = data["key"]
-            if key == config.sec_key:
-                print(get_timestamp(), "Alert Received & Sent!")
+            if data:
+                key = data["key"]
+                if key == config.sec_key:
+                    print(get_timestamp(), "Alert Received & Sent!")
+                    send_alert(data)
+                    return "Sent alert", 200
+                else:
+                    print("[X]", get_timestamp(), "Alert Received & Refused! (Wrong Key)")
+                    return "Refused alert", 400
+            else:
+                data["msg"] = request.get_data(as_text=True)
                 send_alert(data)
                 return "Sent alert", 200
-
-            else:
-                print("[X]", get_timestamp(), "Alert Received & Refused! (Wrong Key)")
-                return "Refused alert", 400
 
     except Exception as e:
         print("[X]", get_timestamp(), "Error:\n>", e)
